6e0d582af07be419e51874cd37fe0891f880b444
mugheesahmad16 <mughees.malik1124@gmail.com>
2024-12-13 01:24:55 +0100

    changes revert for testing

--- src/main/java/org/apache/commons/cli/Option.java
+++ src/main/java/org/apache/commons/cli/Option.java
@@ -570,9 +570,9 @@ public class Option implements Cloneable, Serializable {
     @Override
     public Object clone() {
         try {
-            final Option optionclass = (Option) super.clone();
-            optionclass.values = new ArrayList<>(values);
-            return optionclass;
+            final Option option = (Option) super.clone();
+            option.values = new ArrayList<>(values);
+            return option;
         } catch (final CloneNotSupportedException e) {
             throw new UnsupportedOperationException(e.getMessage(), e);
         }
@@ -693,7 +693,7 @@ public class Option implements Cloneable, Serializable {
 
     /**
      * Gets the version when this option was added.
-     *
+     * 
      * @return the version when this option was added, or {@code null} if not set.
      */
     public String getSince() {
